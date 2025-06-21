84eb1e717f8221c03c82441ec95c07bc72338fa5
llajas <29675370+llajas@users.noreply.github.com>
2025-06-21 15:34:27 -0500

    refactor: replace op document get command with opDoc for kubeconfig fetching

--- dot_dotfiles/exports/kubeContext.zsh
+++ dot_dotfiles/exports/kubeContext.zsh
@@ -17,7 +17,7 @@ else
   fi
 
   # Use op to fetch the document and write it to the kubeconfig path
-  op document get '3pmmbxpwty7zvxinnvlsujww44' > "$KUBECONFIG_PATH"
+  opDoc '3pmmbxpwty7zvxinnvlsujww44' 'kubeconfig.yaml' "$KUBECONFIG_PATH"
 
   # Set proper permissions for the kubeconfig file
   chmod 600 "$KUBECONFIG_PATH"
