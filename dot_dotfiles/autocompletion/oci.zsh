6e47fcbacb62567a55a251f4151194240ec64b67
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2025-07-06 22:12:15 -0500

    add OCI autocomplete

---
+++ dot_dotfiles/autocompletion/oci.zsh
@@ -0,0 +1,4 @@
+
+export PATH=/home/$USER/.local/bin:$PATH
+
+[[ -e "/home/$USER/lib/oracle-cli/lib/python3.11/site-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/home/$USER/lib/oracle-cli/lib/python3.11/site-packages/oci_cli/bin/oci_autocomplete.sh"
